9d3b3c5e2d439b9af9383ceed06fbacad6f520ee
kleopatra chatziprimou <chatzipr@kleopatrasMBP2.station>
2020-06-05 17:00:00 +0300

    leetcode 001 - two sum

---
+++ leetcode_001.py
@@ -0,0 +1,20 @@
+def two_sum(nums, target):
+    """
+
+    :param nums: List[int]
+    :param target:  int
+    :return: List[int]
+    """
+    store = {}
+    for idx, num in enumerate(nums):
+
+        if target - num in store:
+
+            return [ store.get(target - num), idx]
+        else:
+            store[num] = idx
+    return []
+
+
+if __name__ == '__main__':
+    print(two_sum([2, 7, 11, 15], 9))
